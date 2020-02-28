220826b14420baebd8a29115e996f977fb8a54fb
Marie Pendley <mariependley@gmail.com>
2020-02-28 12:57:16 -0500

    added spring

---
+++ stegchan/src/main/java/sec/info/stegchan/StegchanApplication.java
@@ -0,0 +1,13 @@
+package sec.info.stegchan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StegchanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StegchanApplication.class, args);
+	}
+
+}
---
+++ stegchan/src/test/java/sec/info/stegchan/StegchanApplicationTests.java
@@ -0,0 +1,13 @@
+package sec.info.stegchan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StegchanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
