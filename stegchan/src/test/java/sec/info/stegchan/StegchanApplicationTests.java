553e2cd78bb22b03c04229b7610badfeb35e765d
Thomas Sullivan <sullivan.973@osu.edu>
2020-02-29 12:30:09 -0500

    set up repo interfaces

--- stegchan/src/test/java/sec/info/stegchan/StegchanApplicationTests.java
+++ stegchan/src/test/java/sec/info/stegchan/StegchanApplicationTests.java
@@ -1,15 +1,15 @@
-package sec.info.stegchan;
-
-import jdk.nashorn.internal.ir.annotations.Ignore;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class StegchanApplicationTests {
-
-	@Test
-	@Ignore
-	void contextLoads() {
-	}
-
-}
+//package sec.info.stegchan;
+//
+//import jdk.nashorn.internal.ir.annotations.Ignore;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class StegchanApplicationTests {
+//
+//	@Test
+//	@Ignore
+//	void contextLoads() {
+//	}
+//
+//}
