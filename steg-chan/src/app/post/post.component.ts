95740b31a5f056a19bf508bd38963daf21a8dfe6
Thomas Sullivan <sullivan.973@osu.edu>
2020-03-05 15:08:37 -0500

    Oops these too

--- steg-chan/src/app/post/post.component.ts
+++ steg-chan/src/app/post/post.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ThreadData } from '../thread-modal/thread-data';
 
 @Component({
   selector: 'app-post',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.scss']
 })
 export class PostComponent implements OnInit {
-
+  @Input() postData : ThreadData;
   constructor() { }
 
   ngOnInit() {
---
+++ steg-chan/src/app/thread-modal/thread-data.ts
@@ -0,0 +1,9 @@
+export class ThreadData {
+
+  constructor(
+    public title?: string,
+    public message?: string,
+    public imageBase64DataUrl?: string
+  ) {  }
+
+}
